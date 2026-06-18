80e3cfdc157eeb00722b50a296dac2f7082411a4
faiznrw745-cell <faiznrw745@gmail.com>
2026-06-18 12:18:40 +0700

    tambah data

--- fizweekly/mahasiswa.php
+++ fizweekly/mahasiswa.php
@@ -1,4 +1,40 @@
-<!DOCTYPE .php>
+<?php
+
+// API
+$koneksi = mysqli_connect("localhost", "root", "", "fizweekly");
+if($koneksi)
+    {
+        echo "koneksi berhasil";
+    }
+
+    $query = "SELECT * FROM mahasiswa";
+
+    $result = mysqli_query($koneksi, $query); //lemari isi data
+
+    // while ($mhs = mysqli_fetch_assoc($result));
+    // {
+        // var_dump($mhs);
+
+    // }
+
+
+    //ambil data (fetch) dari lemari
+    // mysqli_fetc_row
+    // mysqli_fetc_assoc
+    // mysqli_fetc_array
+    // mysqli_fetc_object
+
+    //var_dump($result);
+
+
+
+?>
+
+
+
+
+
+<!DOCTYPE .html>
 
 <head>
     <meta charset="UTF-8">
@@ -39,28 +75,27 @@
         <tr>
 
         </tr>
+<?php
+while($mhs = mysqli_fetch_assoc($result)) 
+    {
+?>
         <tr>
             <td align="center">1</td>
-            <td>Niki Lauda</td>
-            <td>13242520018</td>
-            <td><img src="assets/images/foto.jpg" width="70" alt="Foto Niki"></td>
-            <td align="center">Teknologi Informasi</td>
-            <td align="center">faiz@example.com</td>
-            <td align="center">0812345678</td>
+            <td><?php echo $mhs["nama"] ?></td>
+            <td><?php echo $mhs["nim"] ?></td>
+            <td><img src="assets/images/<?php echo $mhs["nama"] ?>"></td>
+            <td align="center"><?php echo $mhs["jurusan"] ?></td>
+            <td align="center"><?php echo $mhs["email"] ?></td>
+            <td align="center"><?php echo $mhs["no_hp"] ?></td>
             <td>
                 <a href="editdata.php"><button>Edit</button></a>
                 <a href="deletedata.php"><button>Hapus</button></a>
             </td>
         </tr>
-        <tr>
-            <td align="center">2</td>
-            <td>Niki Lauda</td>
-            <td>13242520018</td>
-            <td><img src="assets/images/foto.jpg" width="70" alt="Foto Niki"></td>
-            <td align="center">50</td>
-            <td align="center">90</td>
-            <td align="center">60</td>
-        </tr>
+        <?php
+    }
+    ?>
+       
     </table>
     
     <hr/>
